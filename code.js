2176fdf530ecf99e8fb002437e020d6f01525300
msislam2308 <msislam2308@gmail.com>
2025-08-01 00:14:49 +0600

    not perfect

--- code.js
+++ code.js
@@ -1,29 +1,32 @@
-let life = 3;
 let score = 0;
-function play() {
+let life = 3;
+function play(){
     document.querySelector('.home').classList.add('hidden');
     document.querySelector('.play').classList.remove('hidden');
-    document.querySelector('.result').classList.add('hidden');
+    console.log('Game Started');
     continueGame();
 }
 function randomLetter() {
-    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ/';
     return letters[Math.floor(Math.random() * letters.length)];
 }
-function setKeyBG(letter) {
-    const element = document.getElementById(letter.toUpperCase());
-    element.classList.add('bg-green-400');
+function setLetterBg(letter) {
+    const element = document.getElementById(letter.toLowerCase());
+    element.classList.add('bg-orange-600');
 }
-function resetKeyBG(letter) {
-    const element = document.getElementById(letter.toUpperCase());
-    element.classList.remove('bg-green-400');
+function resetLetterBg(letter) {
+    const element = document.getElementById(letter.toLowerCase());
+    element.classList.remove('bg-orange-600');
 }
+
 function continueGame() {
     const letter = randomLetter();
     document.querySelector('.letter').textContent = letter;
-    console.log('Displayed:', letter);
-    setKeyBG(letter);
+    console.log("Current Letter: " + letter);
+    setLetterBg(letter);
 }
+
+
 function handleKeyButtonClick(event) {
     const pressedKey = event.key.toUpperCase();
     const displayedLetter = document.querySelector('.letter').textContent;
@@ -32,12 +35,12 @@ function handleKeyButtonClick(event) {
         score += 1;
         console.log('Score:', score);
         document.querySelector('.score').textContent = ` ${score}`;
-        resetKeyBG(displayedLetter);
+        resetLetterBg(displayedLetter);
         continueGame();
     } else {
-        console.log('Incorrect key pressed!You lost a life!');
         life -= 1;
         if (life <= 0) {
+            console.log('Wrong key pressed! You have no lives left!');
             console.log('Game Over');
             document.querySelector('.play').classList.add('hidden');
             document.querySelector('.home').classList.add('hidden');
@@ -48,12 +51,29 @@ function handleKeyButtonClick(event) {
             document.querySelector('.score').textContent = '0';
         }
         else{
+            console.log('Wrong key pressed! You lost a life!');
             console.log('Lives left:', life);
             document.querySelector('.life').textContent = `${life}`;
-            resetKeyBG(displayedLetter);
+            resetLetterBg(displayedLetter);
             continueGame();
         }
     }
 }
 document.addEventListener('keyup', handleKeyButtonClick);
-
+function playAgain() {
+    score = 0;
+    life = 3;
+    document.querySelector('.result').classList.add('hidden');
+    document.querySelector('.play').classList.remove('hidden');
+    console.log('Game Restarted again');
+}
+function reset() {
+    score = 0;
+    life = 3;
+    document.querySelector('.result').classList.add('hidden');
+    document.querySelector('.play').classList.add('hidden');
+    document.querySelector('.home').classList.remove('hidden');
+    document.querySelector('.score').textContent = '0';
+    document.querySelector('.life').textContent = '3';
+    console.log('Game Reset');
+ } 
